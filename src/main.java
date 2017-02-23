dc13e4e7fe5a7a6cea8e4153477e92fbf1f9bfa3
Francis Marsolais <francis.marsolaisv4si@gmail.com>
2017-02-22 23:10:56 -0500

    Modification du main -Fonction de lecture en cours de creation.

--- src/main.java
+++ src/main.java
@@ -1,10 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class main {
 
-	public static void main(){
+	public static void main(String args[]) throws Exception {
 		CCompagnie CO = new CCompagnie();
 		CChauffeur Chauffeur1 = new CChauffeur("Francis","Marsolais","725 Duhamel","2017");
 		Chauffeur1.ToString();
+		
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		
+		System.out.print("S'il vous plaît entrez ville d'arrivée:\n\t");
+		String entre = in.readLine();
+		System.out.print(entre + " est votre choix!");
 	}
 	
 }
